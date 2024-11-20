46398a2d27817cd421dc1a7c659b9b529ba87dd6
alokmishra97 <157737497+alokmishra97@users.noreply.github.com>
2024-11-20 18:15:35 +0530

    Update scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -12,38 +12,41 @@ class TemperatureScraper:
         self.url = url
 
     def fetch_temperature(self):
-        # Setup WebDriver for Selenium 
+        # Setup WebDriver for Selenium
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')  # Run in headless mode (no UI)
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-        # Navigate to the page
-        driver.get(self.url)
-
-        # Switch to iframe as the temperature element is inside an iframe
         try:
-            # Find iframe element
-            iframe = driver.find_element(By.TAG_NAME, "iframe")
-            driver.switch_to.frame(iframe)
-            print("Switched to iframe.")
-        except Exception as e:
-            print("No iframe found or switching to iframe failed.")
+            # Navigate to the page
+            driver.get(self.url)
 
-        try:
-            # Wait up to 20 seconds for the temperature element to be visible
-            print("Waiting for temperature element...")
-            temperature_element = WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.ID, "ajaxtemp"))
-            )
-            print("Temperature element found!")
+            # Switch to iframe as the temperature element is inside an iframe
+            try:
+                # Find iframe element
+                iframe = driver.find_element(By.TAG_NAME, "iframe")
+                driver.switch_to.frame(iframe)
+                print("Switched to iframe.")
+            except Exception as e:
+                print("No iframe found or switching to iframe failed.")
 
-            # Extract and clean the temperature value
-            temperature = float(temperature_element.text.strip().replace('°C', '').replace(',', '.'))
+            try:
+                # Wait up to 20 seconds for the temperature element to be visible
+                print("Waiting for temperature element...")
+                temperature_element = WebDriverWait(driver, 20).until(
+                    EC.visibility_of_element_located((By.ID, "ajaxtemp"))
+                )
+                print("Temperature element found!")
 
-        except Exception as e:
-            driver.quit()
-            raise Exception(f"Error retrieving temperature: {e}")
+                # Extract and clean the temperature value
+                temperature = float(temperature_element.text.strip().replace('°C', '').replace(',', '.'))
 
-        driver.quit()  # Close the browser
-        return temperature
+            except Exception as e:
+                raise Exception(f"Error retrieving temperature: {e}")
+
+            return temperature
+
+        finally:
+            driver.quit()  # Ensure the driver is always quit
+
 
