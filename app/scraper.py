5f5f3ab3afe42b52ac3f9090f50798b5b7f62133
alokmishra97 <157737497+alokmishra97@users.noreply.github.com>
2024-11-20 19:24:41 +0530

    Update scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -14,7 +14,12 @@ class TemperatureScraper:
     def fetch_temperature(self):
         # Setup WebDriver for Selenium
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')  # Run in headless mode (no UI)
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("blink-settings=imagesEnabled=false")
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         try:
